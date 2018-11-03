9db982f19e99f47b18bb02c999f1e16c54a0015d
Gabriel Rohden <gabriel_rohden@outlook.com>
2018-11-03 15:47:33 -0300

    Removed appbar and added true fullscreen

--- lib/ui/nice_fullscreen_gallery.dart
+++ lib/ui/nice_fullscreen_gallery.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nice_animals_flutter/data/nice_picture/nice_picture.dart';
 
 class NiceFullScreenGallery extends StatefulWidget {
@@ -21,29 +22,30 @@ class _NiceFullScreenGalleryState extends State<NiceFullScreenGallery> {
 
   _NiceFullScreenGalleryState(this.initialPage, this.list) : super() {
     this.controller = PageController(initialPage: initialPage);
+    SystemChrome.setEnabledSystemUIOverlays([]);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        leading: CloseButton(),
-        iconTheme: IconThemeData(color: Colors.white),
-        backgroundColor: Colors.transparent,
-        elevation: 0.7,
-      ),
-      body: PageView.builder(
-        controller: controller,
-        physics: AlwaysScrollableScrollPhysics(),
-        itemBuilder: (BuildContext context, int index) {
-          return CachedNetworkImage(
-            imageUrl: list[index].url,
-            placeholder: Center(child: CircularProgressIndicator()),
-            errorWidget: Image.asset("assets/image_not_found.png"),
-            fit: BoxFit.fitWidth,
-          );
-        },
+    return WillPopScope(
+      onWillPop: () {
+        SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+        Navigator.pop(context, true);
+      },
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: PageView.builder(
+          controller: controller,
+          physics: AlwaysScrollableScrollPhysics(),
+          itemBuilder: (BuildContext context, int index) {
+            return CachedNetworkImage(
+              imageUrl: list[index].url,
+              placeholder: Center(child: CircularProgressIndicator()),
+              errorWidget: Image.asset("assets/image_not_found.png"),
+              fit: BoxFit.contain,
+            );
+          },
+        ),
       ),
     );
   }
